5f0ac7e512f981d1e0a5666010c54913490eff8b
Hiye Shin <hiyeshin@gmail.com>
2013-06-06 23:57:15 -0400

    temporary splash jpg

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
 import os, datetime
+import re
+from unidecode import unidecode
 
-from flask import Flask, request, jsonify # Retrieve Flask, our framework
-from flask import render_template
+from flask import Flask, session, request, url_for, escape, render_template, json, jsonify, flash, redirect, abort
+# session is just a dictionary and flask converts it to cookie
+import models
 
+from flask.ext.mongoengine import mongoengine
 
-#import requests # used to make calls to remote youtube api
+import requests
+
+from flaskext.bcrypt import Bcrypt
+
+#custom user library - maps User object to User model
+# from libs.user import *
 
 app = Flask(__name__)   # create our flask app
+app.config['CSRF_ENABLED'] = False
+app.secret_key = os.environ.get('SECRET_KEY')
+
+flask_bcrypt = Bcrypt(app)
+
+#  MONGOLAB_URI=mongodb://localhost:27017/dwdfall2012
+
+mongoengine.connect('mydata', host=os.environ.get('MONGOLAB_URI'))
+
 
 # this is our main page
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
+
 def index():
 	return render_template("main.html")
 
 @app.route("/about")
 def boston():
 	return render_template("about.html")
-
 
 
 
---
+++ forms.py
@@ -0,0 +1,6 @@
+from flaskext.wtf import Form, TextField, Required, PasswordField, validators, SelectField, EqualTo
+from flaskext.wtf.html5 import EmailField
+from flask import Flask, session
+
+class RegisterForm(Form):
+	email = EmailField('Email Address', validators=[], description="Enter your email address.")
