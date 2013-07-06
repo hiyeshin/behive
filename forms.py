e6d0576a2f9f2aceb89cdb2f62461849660198d7
Hiye Shin <hiyeshin@gmail.com>
2013-07-06 06:28:02 -0400

    fixed form size

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
-from flaskext.wtf import Form, TextField, Required, PasswordField, validators, SelectField, EqualTo
+from flaskext.wtf import Form, TextField, TextAreaField, Required, PasswordField, validators, SelectField, EqualTo
 from flaskext.wtf.html5 import EmailField
 from flask import Flask, session
 
 class RegisterForm(Form):
 	email = EmailField('Email Address', validators=[], description="Enter your email address.")
-	first = TextField('First Name', validators = [], description = "First Name")
-	last = TextField('Last Name', validators = [], description = "Last Name")
+	# first = TextField('First Name', validators = [], description = "First Name")
+	# last = TextField('Last Name', validators = [], description = "Last Name")
