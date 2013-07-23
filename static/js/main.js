47c1386699050f1028d3f5f5bec6f28b9d3a00a6
Hiye Shin <hiyeshin@gmail.com>
2013-07-22 22:09:52 -0400

    code tweak

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
-$('.dropdown a').click(function(e) {
-  e.preventDefault();
-  setTimeout($.proxy(function() {
-    if ('ontouchstart' in document.documentElement) {
-      $(this).siblings('.dropdown-backdrop').off().remove();
-    }
-  }, this), 0);
-});
+// $('.dropdown a').click(function(e) {
+//   e.preventDefault();
+//   setTimeout($.proxy(function() {
+//     if ('ontouchstart' in document.documentElement) {
+//       $(this).siblings('.dropdown-backdrop').off().remove();
+//     }
+//   }, this), 0);
+// });
